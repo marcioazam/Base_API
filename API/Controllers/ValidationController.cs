db1d6e9b13ea4298187f9c9b6ac090c93417b9e8
Márcio Contreiras <marcio_contreiras@outlook.com.br>
2024-09-06 00:42:55 -0300

    - Melhorada a busca da Entity, utilizando um ConcurrentDictionary assim fazendo um cache, armazenando resultados para futuras consultas. Evitando assim que em toda requisição seja feita via reflection. - Verificação se o Type é NULL quando feito a busca via Reflection nas Entitys - Verificação se a extração de rules aconteceu como esperado, caso não, retornando um Bad Request

--- API/Controllers/ValidationController.cs
+++ API/Controllers/ValidationController.cs
@@ -2,6 +2,7 @@
 using Application.Factories;
 using Application.Interfaces.Factories;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 
 namespace API.Controllers
 {
@@ -10,14 +11,30 @@ namespace API.Controllers
     public class ValidationController(IValidationFactory validationFactory) : Controller
     {
         private readonly IValidationFactory _validationFactory = validationFactory;
-
+        private static readonly ConcurrentDictionary<string, Type> TypeCache = new();
 
         [HttpGet]
         public ActionResult<List<ValidationRuleDTO>> ValidationRulesModel(string className)
         {
-            className = "Domain.Models." + className;
+            className = "Domain.Entities." + className;
 
-            var type = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).FirstOrDefault(t => t.FullName == className);
+            // Utilizamos um ConcurrentDictionary para armazenar o mapeamento entre os nomes das classes e os tipos correspondentes.
+            // Isso garante que a busca pelo tipo seja feita apenas uma vez por classe e que o resultado seja armazenado em cache para futuras requisições.
+            var type = TypeCache.GetOrAdd(className, key =>
+            {
+                #pragma warning disable CS8603 // Possível retorno de referência nula.
+                return AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(a => a.GetTypes())
+                    .FirstOrDefault(t =>
+                    {
+                        if (t is null)
+                        {
+                            throw new ArgumentNullException(nameof(t));
+                        }
+                        return t.FullName == key;
+                    });
+                #pragma warning restore CS8603 // Possível retorno de referência nula.
+            });
 
             if (type == null)
             {
@@ -29,6 +46,11 @@ namespace API.Controllers
             #pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
 
             var rules = method.Invoke(_validationFactory, null) as List<ValidationRuleDTO>;
+
+            if (rules == null)
+            {
+                return BadRequest("Falha ao extrair regras de validação.");
+            }
 
             return Ok(rules);
         }
